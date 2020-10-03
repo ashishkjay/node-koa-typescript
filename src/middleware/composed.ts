8d62fa901d59ebfd306025a42804cfcf0dd8484f
Ashish Jayan <ashishkjay@gmail.com>
2020-10-03 12:45:47 -0500

    chore(package.json): removed unused deps

--- src/middleware/composed.ts
+++
@@ -1,7 +0,0 @@
-// import compose from "koa-compose";
-// import helmet from "koa-helmet";
-// import cors from "@koa/cors";
-// import logger from "./logger";
-// import bodyParser from "koa-bodyparser";
-
-// export const composedMiddleware =  compose([helmet(),cors(), logger, bodyParser()]);
