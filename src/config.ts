8be026e671561637d9cad025f529f95ea118bf0c
Ashish Jayan <ashishkjay@gmail.com>
2021-03-31 20:06:27 -0400

    feat(start): added build setup

--- src/config.ts
+++ src/config.ts
@@ -3,28 +3,28 @@ import dotenv from "dotenv";
 dotenv.config({ path: ".env" });
 
 export interface Config {
-    port: number;
-    debugLogging: boolean;
-    dbsslconn: boolean;
-    jwtSecret: string;
-    databaseUrl: string;
-    dbEntitiesPath: string[];
-    cronJobExpression: string;
+  port: number;
+  debugLogging: boolean;
+  dbsslconn: boolean;
+  jwtSecret: string;
+  databaseUrl: string;
+  dbEntitiesPath: string[];
+  cronJobExpression: string;
 }
 
 const isDevMode = process.env.NODE_ENV == "development";
 const isSsl = (process.env.SSL_DATABASE == "true");
 
 const config: Config = {
-    port: +(process.env.PORT || 3000),
-    debugLogging: isDevMode,
+  port: +(process.env.PORT || 3000),
+  debugLogging: isDevMode,
   dbsslconn: isSsl,
-    jwtSecret: process.env.JWT_SECRET || "your-secret-whatever",
-    databaseUrl: process.env.DATABASE_URL || "postgres://user:pass@localhost:5432/apidb",
-    dbEntitiesPath: [
-      ... isDevMode ? ["src/entity/**/*.ts"] : ["build/src/entity/**/*.js"],
-    ],
-    cronJobExpression: "0 * * * *"
+  jwtSecret: process.env.JWT_SECRET || "your-secret-whatever",
+  databaseUrl: process.env.DATABASE_URL || "postgres://user:pass@localhost:5432/apidb",
+  dbEntitiesPath: [
+    ...isDevMode ? ["src/entity/**/*.ts"] : ["build/src/entity/**/*.js"],
+  ],
+  cronJobExpression: "0 * * * *"
 };
 
 export { config };
